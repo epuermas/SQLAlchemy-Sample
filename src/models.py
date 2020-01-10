fdf99619b6d527fb8e5791902de3820b4505a329
Eduardo Puermas <eduardopuermas@hotmail.com>
2020-01-10 01:52:13 +0000

    Initial Commit

--- src/models.py
+++ src/models.py
@@ -8,23 +8,52 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    user_name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    password = Column(String(50), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Profile(Base):
+    __tablename__ = 'profile'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    profile_picture = Column(String(250), nullable=False)
+    biography = Column(String(250))
+    follower_count = Column(Integer)
+    following_count = Column(Integer)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True)
+    content = Column(String(250), nullable=False)
+    comments = Column(String(250))
+    likes = Column(Integer)
+    user = relationship(User)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    
+class Story(Base):
+    __tablename__ = 'story'
+    id = Column(Integer, primary_key=True)
+    content = Column(String(250), nullable=False)
+    user_name = Column(String(250), nullable=False)
+    comments = Column(String(250))
+
+class Comments(Base):
+    __tablename__ = 'comments'
+    id = Column(Integer, primary_key=True)
+    comments = Column(String(250))
+    reply = Column(String(250))
+
+class DirectMessage(Base):
+    __tablename__ = 'direct message'
+    id = Column(Integer, primary_key=True)
+    search = Column(String(250))
+    conversation = Column(String)
 
     def to_dict(self):
         return {}
